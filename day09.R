d6e1582ea0b6549185046ab88b9235f5d6ce67b8
Tom Jemmett <thomas.jemmett@nhs.net>
2019-12-11 11:12:37 +0000

    modifies day 9 solution
    removed the immediate output and made the computer store all output into a list.
    
    removed the extra function for running each cycle and put inside the loop, hoped that this would improve speed but it didn't much
    
    added a function to figure out where to store each functions value to simplify the logic
    
    it's now running in about 3 minutes instead of 6 on my machine

--- day09.R
+++ day09.R
@@ -1,5 +1,5 @@
 # --- Day 9: Sensor Boost ---
-  
+
 library(tidyverse)
 
 options(digits = 22)
@@ -60,7 +60,7 @@ intcode_create <- function(data) {
     op_list = data,
     program_counter = 0,
     state = "not run",
-    output = NA,
+    output = numeric(0),
     input = list(),
     cycles = 0,
     relmode_ptr = 0
@@ -84,129 +84,121 @@ intcode_add_input <- function(intcode, ...) {
       " (Rel Mode: ", intcode$relmode_ptr, ")\n", sep = "")
   cat("op_list:\n")
   print(intcode$op_list, p = intcode$program_counter)
-  cat("Output: ", intcode$output, "\n")
+  cat("Output: \n")
+  print(intcode$output)
   cat("Input:\n")
   print(intcode$input)
 }
 
-intcode_run_cycle <- function(intcode, immediate_output = FALSE) {
-  op_list <- intcode$op_list
-  p <- intcode$program_counter
-  r <- intcode$relmode_ptr
-  intcode$state <- "running"
-  
-  if (op_list[p] == 99) {
-    intcode$state <- "complete"
-    return(intcode)
-  }
-  
-  instr <- op_list[p] %% 100
-  
-  mode_fns <- list(
-    "i" = function(pc) op_list[pc],
-    "p" = function(pc) op_list[op_list[pc]],
-    "r" = function(pc) op_list[op_list[pc], r]
-  )
-  
-  mode_fn_select <- function(pos) {
-    v <- floor((op_list[p] %% (pos*10)) / pos)
-    case_when(v == 0 ~ "p",
-              v == 1 ~ "i",
-              v == 2 ~ "r")
-  }
-  
-  mode_a <- mode_fns[[mode_fn_select(  100)]]
-  mode_b <- mode_fns[[mode_fn_select( 1000)]]
-  # lets not use get the function for c, we need to assign
-  mode_c <- mode_fn_select(10000)
-  
-  new_val <- as.numeric(NA)
-  if (instr == 1) {
-    new_val <- mode_a(p+1) + mode_b(p+2)
-    nvp <- p + 3
-    p <- p + 4
-  } else if (instr == 2) {
-    new_val <- mode_a(p+1) * mode_b(p+2)
-    nvp <- p + 3
-    p <- p + 4
-  } else if(instr == 3) {
-    mode_c <- mode_fn_select(100)
-    new_val <- intcode$input[[1]]
-    intcode$input <- intcode$input[-1]
-    nvp <- p + 1
-    p <- p + 2
-  } else if(instr == 4) {
-    intcode$output <- mode_a(p+1)
-    if (immediate_output) cat(">", intcode$output, "\n")
-    intcode$state <- "interupted"
-    p <- p + 2
-  } else if(instr == 5) {
-    if (mode_a(p+1) != 0) {
-      p <- mode_b(p+2)
-    } else {
-      p <- p + 3
-    }
-  } else if(instr == 6) {
-    if (mode_a(p+1) == 0) {
-      p <- mode_b(p+2)
-    } else {
-      p <- p + 3
-    }
-  } else if(instr == 7) {
-    new_val <- as.numeric(mode_a(p+1) <  mode_b(p+2))
-    nvp <- p + 3
-    p <- p + 4    
-  } else if(instr == 8) {
-    new_val <- as.numeric(mode_a(p+1) == mode_b(p+2))
-    nvp <- p + 3
-    p <- p + 4    
-  } else if (instr == 9) {
-    intcode$relmode_ptr <- r + mode_a(p+1)
-    p <- p + 2
-  } else {
-    print(intcode)
-    stop("unkown op code")
-  }
-  
-  if (!is.na(new_val)) {
-    if (mode_c == "r") {
-      op_list[op_list[nvp], r] <- new_val
-    } else if (mode_c == "p") {
-      op_list[op_list[nvp]] <- new_val
-    } else {
-      op_list[nvp] <- new_val
-    }
-  }
-  
-  intcode$op_list <- op_list
-  intcode$program_counter <- p
-  intcode$cycles <- intcode$cycles + 1
-  
-  return(intcode)
-}
-
 intcode_run <- function(intcode,
                         completion = TRUE,
-                        immediate_output = FALSE ,
                         n = Inf) {
-  cycle <- 0
-  while (intcode$state != "complete" & cycle < n) {
-    intcode <- intcode_run_cycle(intcode, immediate_output)
+  
+  while (intcode$state != "complete" & intcode$cycles < n) {
+    op_list <- intcode$op_list
+    p <- intcode$program_counter
+    r <- intcode$relmode_ptr
+    intcode$state <- "running"
+    
+    if (op_list[p] == 99) {
+      intcode$state <- "complete"
+      break
+    }
+    
+    instr <- op_list[p] %% 100
+    
+    mode_fns <- list(
+      "i" = function(pc) op_list[pc],
+      "p" = function(pc) op_list[op_list[pc]],
+      "r" = function(pc) op_list[op_list[pc], r]
+    )
+    
+    mode_fn_select <- function(pos) {
+      v <- floor((op_list[p] %% (pos*10)) / pos)
+      case_when(v == 0 ~ "p",
+                v == 1 ~ "i",
+                v == 2 ~ "r")
+    }
+    
+    mode_a <- mode_fns[[mode_fn_select(  100)]]
+    mode_b <- mode_fns[[mode_fn_select( 1000)]]
+    # lets not use get the function for c, we need to assign
+    mode_c <- mode_fn_select(10000)
+    
+    save_to <- function(o) {
+      if (mode_c == "i") return (p+o)
+      i <- op_list[p+o]
+      if (mode_c == "r") {
+        i <- i + r
+      }
+      return (i)
+    }
+    
+    new_val <- as.numeric(NA)
+    if (instr == 1) {
+      op_list[save_to(3)] <- mode_a(p+1) + mode_b(p+2)
+      p <- p + 4
+    } else if (instr == 2) {
+      op_list[save_to(3)] <- mode_a(p+1) * mode_b(p+2)
+      p <- p + 4
+    } else if(instr == 3) {
+      mode_c <- mode_fn_select(100)
+      op_list[save_to(1)] <- intcode$input[[1]]
+      intcode$input <- intcode$input[-1]
+      p <- p + 2
+    } else if(instr == 4) {
+      intcode$output <- c(intcode$output, mode_a(p+1))
+      intcode$state <- "interupted"
+      p <- p + 2
+    } else if(instr == 5) {
+      if (mode_a(p+1) != 0) {
+        p <- mode_b(p+2)
+      } else {
+        p <- p + 3
+      }
+    } else if(instr == 6) {
+      if (mode_a(p+1) == 0) {
+        p <- mode_b(p+2)
+      } else {
+        p <- p + 3
+      }
+    } else if(instr == 7) {
+      op_list[save_to(3)] <- as.numeric(mode_a(p+1) <  mode_b(p+2))
+      p <- p + 4    
+    } else if(instr == 8) {
+      op_list[save_to(3)] <- as.numeric(mode_a(p+1) == mode_b(p+2))
+      p <- p + 4    
+    } else if (instr == 9) {
+      intcode$relmode_ptr <- r + mode_a(p+1)
+      p <- p + 2
+    } else {
+      print(intcode)
+      stop("unkown op code")
+    }
+    
+    intcode$op_list <- op_list
+    intcode$program_counter <- p
+    intcode$cycles <- intcode$cycles + 1
+    
     if (!completion && intcode$state == "interupted") break
-    cycle <- cycle + 1
   }
+  
   intcode
 }
 
 intcode_output <- function(intcode) intcode$output
 
 
-# --- Part One ---
-
 # examples
 
-intcode_create(c(109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99)) %>%
-  intcode_run(TRUE, TRUE)
+local({
+  data <- c(109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99)
+  output <- intcode_create(data) %>%
+    intcode_run() %>%
+    intcode_output()
+  
+  all(output == data)
+})
 
 intcode_create(c(1102,34915192,34915192,7,4,7,99,0)) %>%
   intcode_run() %>%
@@ -222,7 +214,7 @@ intcode_create(c(104,1125899906842624,99)) %>%
 part_one <- dt %>%
   intcode_create() %>%
   intcode_add_input(1) %>%
-  intcode_run(TRUE, TRUE)
+  intcode_run()
 
 part_one %>%
   intcode_output()
@@ -233,8 +225,7 @@ tic()
 part_two <- dt %>%
   intcode_create() %>%
   intcode_add_input(2) %>%
-  # this is going to take around 5 minutes on a 4GHz i9...
-  intcode_run(TRUE, TRUE)
+  intcode_run()
 toc()
 
 part_two %>%
