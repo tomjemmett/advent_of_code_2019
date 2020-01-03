6380ffc3701986f46759075dc45d6465aac9e069
Tom Jemmett <thomas.jemmett@nhs.net>
2020-01-03 11:46:46 +0000

    fixes issue with part 2 not giving correct answer by borrowing another solution from Reddit

--- day18.R
+++ day18.R
@@ -1,134 +1,165 @@
 # --- Day 18: Many-Worlds Interpretation ---
 
+# I have rewrote my solution based using a Python solution from Reddit. This
+# solves the problem in a reasonable time in R.
+# https://www.reddit.com/r/adventofcode/comments/ec8090/2019_day_18_solutions/fbagnxw/
+
 library(tidyverse)
 library(datastructures)
-library(memoise)
-library(Rcpp)
 library(compiler)
-library(tictoc)
 
+# read in the data as a matrix
 dt <- read_lines("day18.txt") %>%
   str_split("", simplify = TRUE)
 
-# implements the reachable_keys function: this is just way, way too slow in R,
-# so I've implemented it in C++
-sourceCpp("day18.cpp")
+# I'm using compiler::cmpfun to try to eek out a bit more performance from R.
 
-# use cmpfun to try to eek out any performance improvements by JIT compiling
-# maze should be a matrix of the input, pos should be a list for the starting
-# positions for each of the robots
-solve <- cmpfun(function(maze, pos) {
-  # memoise the reachable_keys function (if we run this function twice with the
-  # same input then it will return the previous results)
-  reachable_keys_mf <- memoise(reachable_keys)
-  
-  # what are the keys that we need to find?
-  all_keys <- maze[maze %in% letters]
-  
-  # create a priority queue: we will visit nodes based on the distance (smaller
-  # distances are visited first)
-  q <- binomial_heap("numeric")
-  # insert the initial positions into the queue
-  insert(q, 0, list(d = 0, pos = pos, keys = ""))
-  
-  # what keys have been seen by each robot?
-  # 1st list is for the robots
-  # 2nd list is for the y position
-  # 3rd list is for the x position
-  # usage: seen[[p]][[y]][[x]]
-  # the final value is a set of all of the keys seen at that position
-  seen <- map(seq_along(pos),
-              ~map(1:nrow(maze),
-                   ~map(1:ncol(maze),
-                        ~character(0))))
-  # used for printing progress
-  ctr <- 0
-  max_keys <- 1
-  
-  # while we have items in the queue
+# function to calculate the distance from a given location to every other key or
+# door that is reachable in the maze
+distances_from <- cmpfun(function(maze, x, y) {
+  q <- queue()
+  # insert into the queue the initial position: it's route starts as an empty
+  # character vector
+  insert(q, list(list(x = x, y = y, dist = 0, route = character(0))))
+  # hold the route info
+  route_info <- list()
+  # what directions can we move?
+  dirs <- list(c(-1,0),c(1,0),c(0,-1),c(0,1))
+  # while there are items in the queue
   while (size(q) > 0) {
-    # iterate the counter
-    ctr <- ctr + 1
-    
-    # get the current lowest-priority item in the queue
+    # remove the first item out of the queue
     n <- pop(q)[[1]]
-    
-    # output the current counter position and how many keys have been found/the
-    # current distance. show this every time a key is found
-    if (max_keys < length(n$keys)) {
-      max_keys <- length(n$keys)
-      cat("\rctr:", str_pad(ctr, 5),
-          " (keys: ", str_pad(max_keys-1, 2),
-          ", d: ", str_pad(n$d, 4),
-          ")\n", sep = "")
-    } else if (!ctr %% 100) {
-      # every 100 iterations outut the counter if we haven't found a key
-      cat("\rctr:", str_pad(ctr, 5),
-          sep = "")
+    # what character is at this position?
+    c <- maze[n$y, n$x]
+    # if it's a letter or a door, and we have moved some distance
+    if (c %in% c(letters, LETTERS) && n$dist > 0) {
+      # then add this item to the route
+      route_info[[c]] <- n[c("dist", "route")]
+      # update the current route to include this
+      n$route <- c(n$route, c)
     }
-    
-    # if we have found all the keys return the distance (as we are using a
-    # minimum priority queue then the current distance is the minimum possible)
-    if (all(all_keys %in% n$keys)) return (n$d)
-    
-    # iterate over each of the robots
-    for (p in seq_along(n$pos)) {
-      # get the robots current position
-      cx <- n$pos[[p]][[1]]
-      cy <- n$pos[[p]][[2]]
-      
-      # get the current seen keys
-      kv <- paste(n$keys, collapse = "")
-      ks <- seen[[p]][[cy]][[cx]]
-      # check to see if the keys that we have in n already exist for this robot:
-      # in other words, check to make sure we haven't already been at this point
-      # with this robot
-      if (kv %in% ks) next()
-      # mark this position for this robot with these keys as seen
-      seen[[p]][[cy]][[cx]] <- c(ks, kv)
-      
-      # find the reachable keys from this position with the currently see keys
-      rk <- reachable_keys_mf(maze, cx, cy, n$keys[n$keys %in% letters])
-      
-      # iterate over each of the reachable keys
-      for (r in rk) {
-        # update this robots position to be the new reachable key
-        npos <- n$pos
-        npos[[p]] <- c(r$x, r$y)
-        # update the distance to reach this key
-        nd <- n$d + r$l
-        # insert this new position into the priority queue
-        insert(q, nd, list(d    = nd,
-                           pos  = npos,
-                           # ensure that the keys are always sorted in order
-                           # as our keys will already mostly be in the correct
-                           # order we can get away with using a shell sort
-                           keys = sort(c(n$keys, r$c), method = "shell")))
+    # mark this place as visited
+    maze[n$y, n$x] <- "#"
+    # iterate over the areas we can move to
+    for (d in dirs) {
+      nx <- n$x + d[[1]]
+      ny <- n$y + d[[2]]
+      # if the new position isn't a #, then add this position to the queue
+      if (maze[ny, nx] != "#") {
+        insert(q, list(list(x = nx, y = ny, dist = n$dist+1, route = n$route)))
       }
     }
   }
-  # if we don't find a solution, return NA
-  return (as.numeric(NA))
+  # return the route_info
+  route_info
+})
+# find all of the options for movement in the maze from each named position to
+# every other named position
+find_routeinfo <- cmpfun(function(maze) {
+  route_info <- list()
+  # iterate over every cell in the matrix
+  for (y in 1:nrow(maze)) {
+    for (x in 1:ncol(maze)) {
+      c <- maze[y, x]
+      # if this cell is either a key or one of the start location, then work
+      # out the distances to every other position
+      if (c %in% c(letters,"1","2","3","4","@")) {
+        route_info[[c]] <- distances_from(maze, x, y)
+      }
+    }
+  }
+  route_info
 })
 
-# find @
-sx <- (1:ncol(dt))[apply(dt, 2, function(x) any(x == "@"))]
-sy <- (1:nrow(dt))[apply(dt, 1, function(x) any(x == "@"))]
+# function to solve the problem: takes in the route_info for the maze, an
+# initial position, and the keys to find
+solve <- cmpfun(function(route_info, info, keys) {
+  # iterate once for every key
+  for (i in seq_along(keys)) {
+    # create a list for the next iteration
+    nextinfo <- list()
+    # iterate over all of the current items in info
+    for (item in info) {
+      # iterate over each of the keys
+      for (k in keys) {
+        # if this key has already been found then skip
+        if (k %in% item$keys) next()
+        
+        # for each of the robots
+        for (robot in seq_along(item$locs)) {
+          # get the route info to this key
+          ri <- route_info[[item$locs[[robot]]]][[k]]
+          # if this robot cannot reach this key then move to the next robot
+          if (is.null(ri)) next()
+          # check to see if we can reach this key
+          if (all(
+            ri$route %in% item$keys |
+            str_to_lower(ri$route) %in% item$keys
+          )) {
+            # we can reach the key, so update the info object
+            nd <- item$dist + ri$dist
+            # make sure to sort the keys we have found: this is to ensure that
+            # if we reach key a then b, but also b then a, only the shortest
+            # path is considered
+            nk <- sort(c(item$keys, k), method = "shell")
+            # update the location of this robot
+            nl <- item$locs
+            nl[[robot]] <- k
+            # create a key for the info object by combining the robots positions
+            # with the keys found
+            j <- paste(c(nl, "|", nk), collapse = "")
+            # check to see if this key exists in nextinfo: insert it if not,
+            # update it only if the distance is shorter
+            if (is.null(nextinfo[[j]]) || nd < nextinfo[[j]]$dist) {
+              nextinfo[[j]] <- list(locs = nl,
+                                    keys = nk,
+                                    dist = nd)
+            }
+          }
+        }
+      }
+    }
+    # update the info object for the next iteration
+    info <- nextinfo
+  }
+  # pull the "dist" values from the info object and return the minimum
+  map_dbl(info, "dist") %>% min()
+})
 
-# --- Part One ---
-tic()
-part_one <- solve(dt, list(c(sx, sy)))
-toc()
+part_one <- function(maze) {
+  # get the route info for this maze
+  route_info <- find_routeinfo(maze)
+  # find the keys
+  keys <- names(route_info)[names(route_info) %in% letters]
+  # build the initial info for this robot
+  info <- list("@|" = list(locs = "@",
+                           keys = character(0),
+                           dist = 0))
+  # solve
+  solve(route_info, info, keys)
+}
 
-# --- Part Two ---
-dt2 <- dt
-dt2[(sy-1):(sy+1), (sx-1):(sx+1)] <- c("@","#","@","#","#","#","@","#","@")
-tic()
-part_two <- solve(dt2, list(c(sx-1, sy-1),
-                            c(sx+1, sy-1),
-                            c(sx-1, sy+1),
-                            c(sx+1, sy+1)))
-toc()
+part_two <- function(maze) {
+  # update the maze for part two: replace the @ with 1,2,3,4 for each of the
+  # new start positions
+  # first find the @
+  start <- seq_along(maze)[maze == "@"] - 1
+  sy <- start %% nrow(maze) + 1
+  sx <- floor(start / nrow(maze)) + 1
+  # then update the maze
+  maze[(sy-1):(sy+1), (sx-1):(sx+1)] <- c("1","#","3","#","#","#","2","#","4")
+  # get the route info for this maze
+  route_info <- find_routeinfo(maze)
+  # find the keys for this maze
+  keys <- names(route_info)[names(route_info) %in% letters]
+  # build the initial info object for these robots
+  info <- list("1234|" = list(locs = c("1","2","3","4"),
+                              keys = character(0),
+                              dist = 0))
+  # solve
+  solve(route_info, info, keys)
+}
 
-cat("Part One: ", part_one, "\n",
-    "Part Two: ", part_two, "\n", sep = "")
+
+part_one(dt)
+part_two(dt)
